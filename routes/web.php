818943a7367d1eae578dd68d2d93da878a4ecbe6
Humni <nicholas.humphries0@gmail.com>
2017-07-29 09:27:42 +1200

    Setup base bot

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+$app->get('/webhook', 'BotController@verify_token');
+$app->post('/webhook', 'BotController@handle_query');
