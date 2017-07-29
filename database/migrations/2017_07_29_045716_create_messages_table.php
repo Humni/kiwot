cb0d9048cdef16e50a8eed88655d7e0e4887960e
Humni <nicholas.humphries0@gmail.com>
2017-07-29 17:39:37 +1200

    Another migration bug

--- database/migrations/2017_07_29_045716_create_messages_table.php
+++ database/migrations/2017_07_29_045716_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMessages extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
