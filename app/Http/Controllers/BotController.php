33e787e7d89ed70fa81dfe534413627fd9c149bf
Humni <nicholas.humphries0@gmail.com>
2017-07-29 14:18:53 +1200

    getting the correct sender id

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -52,11 +52,11 @@ class BotController extends Controller
         $request = json_decode($request->getContent());
         $entry = $request->entry;
 
-        Log::debug("Sender id" . json_encode($entry));
-        $sender  = $entry[0]->id;
-        Log::debug("Sender id" . $sender);
+        Log::debug("Entry: " . json_encode($entry));
+        $sender  = $entry[0]->messaging[0]->sender->id;
+        Log::debug("Sender id: " . $sender);
         $message = $entry[0]->messaging[0]->message->text;
-        Log::debug("Incoming message" . $message);
+        Log::debug("Incoming message: " . $message);
 
         $this->dispatchResponse($sender, 'Hello world. You can customise my response.');
 
