e37ce7d76647967653410421b74a30a7d09bb28a
Humni <nicholas.humphries0@gmail.com>
2017-07-29 14:58:46 +1200

    updated geophp test

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -8,16 +8,18 @@ use Illuminate\Http\Request;
 
 class DevController extends Controller
 {
-    /**
-     * Verify the token from Messenger. This helps verify your bot.
-     *
-     * @param  Request $request
-     * @return \Illuminate\Http\Response|\Laravel\Lumen\Http\ResponseFactory
-     */
     public function geophp(Request $request)
     {
         $geometry = geoPHP::load('MULTILINESTRING((10 10,20 20,10 40))','wkt');
 
         return response("Test reduce" . PHP_EOL . json_encode($geometry), 200);
+    }
+
+    public function geophp(Request $request)
+    {
+        if (geoPHP::geosInstalled()) {
+            return response("We can't work anything out at the moment!", 200);
+        }
+        return response("GeoPHP is not installed", 400);
     }
 }
