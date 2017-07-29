e7b5d5c28733731bdcb0aae43a461128ba101952
Humni <nicholas.humphries0@gmail.com>
2017-07-29 13:14:15 +1200

    fixed the logging

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -48,8 +48,8 @@ class BotController extends Controller
      */
     public function handle_query(Request $request)
     {
-        Log::debug("Handle Query:: Post data: " . $_POST);
-        Log::debug("Handle Query:: Get data: " . $_GET);
+        Log::debug("Handle Query:: Post data: " . json_encode($_POST));
+        Log::debug("Handle Query:: Get data: " . json_encode($_GET));
         $entry = $_GET['entry'];
 
         $sender  = array_get($entry, '0.messaging.0.sender.id');
