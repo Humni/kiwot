a31fa7888e819e07afe15d19d5b9bd3f00d37010
Humni <nicholas.humphries0@gmail.com>
2017-07-29 15:10:08 +1200

    updated the dev controller

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -10,13 +10,6 @@ class DevController extends Controller
 {
     public function geophp(Request $request)
     {
-        $geometry = geoPHP::load('MULTILINESTRING((10 10,20 20,10 40))','wkt');
-
-        return response("Test reduce" . PHP_EOL . json_encode($geometry), 200);
-    }
-
-    public function geophp(Request $request)
-    {
         if (geoPHP::geosInstalled()) {
             return response("We can't work anything out at the moment!", 200);
         }
